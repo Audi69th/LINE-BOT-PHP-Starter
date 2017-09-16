ce268e76e0b2e0df57003bab4b440d84222400f9
Audi69th <ouddy_sniper@hotmail.com>
2017-09-16 20:44:21 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:fZjAvfOVnbVywJS';
+
 $access_token = '/tGMUg8XbOxnElMSVFnZdMaEyUoIOGVentGWCRnNkY7iwaVp8r25THGwuFNnj7a2fA6PxEE6EE0k2KlzoWg9OLtXXn0SmDLa9CR5RlwrRVhOxt9jddFVnxCh2ZDIANfY2I7fYPiw/Vsbzt89KjFbBgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
